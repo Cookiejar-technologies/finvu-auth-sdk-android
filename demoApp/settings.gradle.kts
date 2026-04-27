45d5ae696a161bec34f892cc798739ca6e5a9b06
Jenil <jenilk@Jenils-MacBook-Pro.local>
2026-04-27 15:41:55 +0530

    demoapp and readme updates as per new flow

--- demoApp/settings.gradle.kts
+++ demoApp/settings.gradle.kts
@@ -19,8 +19,8 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/Cookiejar-technologies/finvu-auth-sdk-android")
             credentials {
-                username = "username"
-                password = "pat"
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("USERNAME") ?: ""
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("TOKEN") ?: ""
             }
         }
     }
